e792cbc09c6f35c0aa664fb1f4f1c6a91d34070a
Oguzhan Gungor <cansingungor@gmail.com>
2013-04-21 17:55:15 +1000

    XCode project added

---
+++ BufferedWriter/BufferedWriter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BufferedWriter
+//
+//  Created by Oguzhan Gungor on 21/04/13.
+//  Copyright (c) 2013 Oguzhan Gungor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
